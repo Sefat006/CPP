4e72ad186f67bc53a2bdc6c050c99364b4690656
sefat006 <sefatur76@gmail.com>
2024-01-12 18:50:22 +0600

    done

--- Data Structure and Algorithm/binary-search.cpp
+++ Data Structure and Algorithm/binary-search.cpp
@@ -1,7 +1,42 @@
 #include<iostream>
 using namespace std;
 
+int binarySearch(int arr[], int size, int key){
+    int start = 0;
+    int end = size - 1;
+
+    int mid = (start+end)/2;
+
+    while(start <= end)
+    {
+        if((arr[mid])==key)
+        {
+            return mid;
+        }
+        
+        if(key > arr[mid])//go to right(mid+1)
+        {
+            start = mid + 1;
+        }
+        else{//go to left(mid-1)
+            end = mid - 1;
+        }
+
+        mid = (start+end)/2;
+    }
+    return -1;
+}
+
 int main(){
     int even[6] = {2,4,6,8,12,18};
-    int odd[5]= {3,8,11,14,16}
+    int odd[5]= {3,8,11,14,16};
+
+    int evenIndex = binarySearch(even, 6, 20);
+    cout<< "index of 12 is "<< evenIndex<<endl;
+
+    int oddIndex = binarySearch(odd, 5, 8);
+    cout<< "index of 8 is "<< oddIndex<<endl;
+
+
+    return 0;
 }
