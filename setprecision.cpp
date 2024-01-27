6268c37d73ef5a7b763cc1620e1a7816491a6754
sefat006 <sefatur76@gmail.com>
2024-01-28 00:19:51 +0600

    setprecision

---
+++ setprecision.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    float f;
+    double l;
+    cout<< "input: ";
+    cin>> f >> l;
+    cout<<showpoint;//weiting only this will show the 0 at last after point; example : 2.3210
+    cout<< setprecision(4);//if we do not use fixed; than it will count 4 including " . ";
+    cout<< fixed;
+    cout<< f << endl;
+    cout<< noshowpoint;
+    cout<< l;
+
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+    cout<< "hello aworl";
+    float f;
+    cin>> f;
+    cout<< showpoint;
+    cout<<setprecision(2);
+    cout<< f;
+    return 0;
+}
