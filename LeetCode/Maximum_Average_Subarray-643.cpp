ee27a781f4738279005a90366015ccb930c58a39
sefat006 <sefatur76@gmail.com>
2025-05-24 02:04:46 +0600

    sliding window

---
+++ LeetCode/Maximum_Average_Subarray-643.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// sliding window technique
+
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double sum = 0;
+
+        for(int i=0; i<k; i++){
+            sum += nums[i];
+        }
+
+        double maxSum = sum;
+
+        for(int i=k; i<nums.size(); i++){
+            sum = sum + nums[i] - nums[i-k];
+            maxSum = max(maxSum, sum);
+        }
+
+        return maxSum/k;
+    }
+        
+};
